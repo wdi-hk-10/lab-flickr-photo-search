39c5f522dbe2e08f769a6513e186cfe28ea5752d
Shawn Alwani <shawn.alwani@gmail.com>
2016-02-04 19:40:24 +0800

    only picture remaining

--- js/app.js
+++ js/app.js
@@ -1,7 +1,52 @@
 $(document).ready(function(){
 
+function emptyScreen(){
+  $("#images1").empty();
+  $("#images2").empty();
+  $("#images3").empty();
+  $("#images4").empty();
+  $("#images5").empty();
+}
+
+function addImages(response) {
+  var counter = 1;
+  var row = 0;
+  response.items.forEach(function(elem){
+    var newImg = "<div class= \"col-xs-3\" data-author="+elem.author+" data-title="+elem.title+" data-date="+elem.date_taken+" data-tags="+elem.tags+"><img class=\"thumbnail\" src="+elem.media.m+"></img></div>";
+    if(counter%4===0) {
+      row=row+1;
+    }
+    counter=counter+1;
+    $("#images"+row.toString()).append(newImg);
+  });
+}
+
+function fillModal(author, title, dateTaken, tags){
+  var leftCol = "<div class=\"col-xs-6 pull-left\">Picture</div>"
+  var rightCol = "<div class=\"col-xs-6 pull-right\"><strong>Title: </strong>"+title+"<br><strong>Author: </strong>"+author+"<br><strong>Date Taken: </strong>"+dateTaken+"<br><strong>Tags: </strong>"+tags+"</div>"
+  $(".modal-body").append(leftCol).append(rightCol);
+}
+
+function bindImages(){
+  $(".thumbnail").on("click", function(){
+    var author=$(this).parent().data("author");
+    var title=$(this).parent().data("title");
+    var dateTaken=$(this).parent().data("date");
+    var tags=$(this).parent().data("tags");
+    var picture=$(this).filter("img");
+    console.log(picture);
+    fillModal(author, title, dateTaken, tags);
+    $("#edit-modal").modal("show");
+  });
+  $("#close").on("click", function(){
+    $(".modal-body").empty();
+  });
+}
+
+
 $("button").on("click",function(e){
   e.preventDefault();
+  emptyScreen();
   var field1 = $(".form-control").val();
   var field2 = $(".selectpicker").val();
   $.ajax({
@@ -14,19 +59,9 @@ $("button").on("click",function(e){
     },
     dataType: 'jsonp',
     success: function (response, status){
-      //APPEND DATA TO HTML
-      var counter = 1;
-      var row = 0;
-      response.items.forEach(function(elem){
-        var newImg = "<div class= \"col-xs-3\"><img class=\"thumbnail\" src="+elem.media.m+"></img></div>";
-        if(counter%4===0) {
-          row=row+1;
-        }
-        counter=counter+1;
-        $("#images"+row.toString()).append(newImg);
-      })
-      //GET REST OF DATA
-
+      console.log(response);
+      addImages(response);
+      bindImages();
     },
     error: function (response, status){
       console.log(response);
