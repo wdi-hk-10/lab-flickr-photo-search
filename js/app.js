289b61b4e2523f6e89714158193365f544de6390
cmtaubman <carmen@cgkcorporation.com>
2016-02-04 17:23:41 +0800

    search function added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 // YOUR CODE HERE
+$(document).ready(function (){
+
+  var getPhoto = function (){
+    $.ajax({
+      url: "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+      method: "GET",
+      data: {
+        format: "json",
+        tags: $(".form-control").val(),
+        tagmode: $(".btn dropdown-toggle btn-default").val(),
+        link: "link"
+      },
+      dataType: 'jsonp',
+      success: function (response, status) {
+        console.log(response);
+        // console.log(response.items);
+        var object = response.items;
+        var newImages = object.forEach(function (data){
+          // console.log(data.media.m);
+          $("<img>").attr("src", data.media.m).appendTo($("#images"));
+        });
+      },
+      error: function (response, status) {
+        console.log(response);
+      }
+    });
+  };
+
+  var searchButton = function () {
+    $("#search").on("click", function(){
+      $("#images").empty();
+      getPhoto();
+    });
+  }
+
+  searchButton();
+});
